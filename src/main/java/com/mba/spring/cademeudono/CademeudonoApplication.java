862c4e577a837159f8cc2982bb0dfe9dd732b757
Rafael <rafa.rp22@gmail.com>
2021-12-10 08:41:32 -0300

    Primeiro commit

---
+++ src/main/java/com/mba/spring/cademeudono/CademeudonoApplication.java
@@ -0,0 +1,13 @@
+package com.mba.spring.cademeudono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CademeudonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CademeudonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mba/spring/cademeudono/CademeudonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mba.spring.cademeudono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CademeudonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
